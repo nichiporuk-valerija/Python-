aab6fe5855f525cb2d9acb7f5e71f93900d2223d
unknown <ambal.07@mail.ru>
2025-02-21 17:11:00 +0300

    first commit

--- Selenium/Mozila_script_1.py
+++ Selenium/Mozila_script_1.py
@@ -3,13 +3,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from time import sleep
 firefox_options = Options()
-driver = webdriver.Firefox
-drivers = webdriver.Firefox
+driver = webdriver.Firefox(options=firefox_options)
 
-sleep(3)
+
 driver.get("http://the-internet.herokuapp.com/entry_ad")
-close_button = driver.find_element(By.XPATH, "//div[@id='modal']")
+sleep(3)
+close_button = driver.find_element(By.CSS_SELECTOR, "div.modal-footer > p")
 driver.execute_script("arguments[0].scrollIntoView();", close_button)
 close_button.click()
 
 sleep(3)
+driver.quit()
+
+
+
